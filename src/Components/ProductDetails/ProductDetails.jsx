0cc2c99b8727dda8989e50ddadfdba1d8a21b86a
hodamagdi <hodamagdi88@gmail.com>
2024-09-11 16:38:31 +0300

    fixing responsive productDetails

--- src/Components/ProductDetails/ProductDetails.jsx
+++ src/Components/ProductDetails/ProductDetails.jsx
@@ -8,12 +8,10 @@ import { CartContext } from "../../Context/CartContext";
 import toast from "react-hot-toast";
 import Slider from "react-slick";
 import { useRef } from "react";
-import { IoMdArrowDroprightCircle , IoMdArrowDropleftCircle } from "react-icons/io";
-
+import { IoMdArrowDroprightCircle, IoMdArrowDropleftCircle } from "react-icons/io";
 
 function ProductDetails() {
   const { addItemToCart, setCartItems } = useContext(CartContext);
-
 
   let sliderRef = useRef(null);
   const next = () => {
@@ -29,26 +27,14 @@ function ProductDetails() {
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
-    arrows: false ,
-    responsive: [
-      {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          initialSlide: 1
-        }
-      },
-
-    ]
-
+    arrows: false,
   };
 
   async function addItem(id) {
     const response = await addItemToCart(id);
     if (response.data.status === "success") {
       setCartItems(response.data.numOfCartItems);
-      toast.success('Added to cart');
+      toast.success("Added to cart");
     }
   }
 
@@ -60,14 +46,11 @@ function ProductDetails() {
     isError,
     error,
   } = useQuery({
-    queryKey: ['productDetails', id],
-    queryFn: () => axios.get(
-      "https://ecommerce.routemisr.com/api/v1/products/" + id
-    ),
+    queryKey: ["productDetails", id],
+    queryFn: () =>
+      axios.get("https://ecommerce.routemisr.com/api/v1/products/" + id),
     select: (data) => data.data.data,
   });
-
-
 
   if (isError) {
     return <div>Error: {error.message}</div>;
@@ -79,49 +62,47 @@ function ProductDetails() {
         <Loading />
       ) : (
         <>
-          <div className="grid sm:grid-cols-12 gap-4 dark:bg-gray-700 bg-gray-100">
-            <div className="col-span-4 p-5">
-              <Slider ref={slider => {sliderRef = slider;}}{...settings}>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 dark:bg-gray-700 bg-gray-100">
+            <div className="md:col-span-1 p-5">
+              <Slider ref={(slider) => (sliderRef = slider)} {...settings}>
                 {productDetails?.images.map((i) => (
                   <div key={i}>
                     <img
-                      src={i} 
+                      src={i}
                       alt="Product Image"
-                      className="col-span-1  w-full object-cover"
+                      className="w-full object-contain object-r"
                     />
                   </div>
                 ))}
               </Slider>
-              <div className="flex  justify-center gap-4 mt-4">
+              <div className="flex justify-center gap-4 mt-4">
                 <button className="button" onClick={previous}>
-                <IoMdArrowDropleftCircle className="text-2xl text-green-600"/>
+                  <IoMdArrowDropleftCircle className="text-2xl md:text-4xl text-green-600" />
                 </button>
                 <button className="button" onClick={next}>
-                <IoMdArrowDroprightCircle className="text-2xl text-green-600" />
+                  <IoMdArrowDroprightCircle className="text-2xl md:text-4xl text-green-600" />
                 </button>
               </div>
-
             </div>
-            <div className="col-span-8 p-5 self-center dark:text-white">
-              <h2>{productDetails?.title}</h2>
-              <p className="my-3 font-light ">{productDetails?.description}</p>
-              <h3 className="mb-2">{productDetails?.category?.name}</h3>
+            <div className="md:col-span-1 lg:col-span-2 p-5 self-center dark:text-white">
+              <h2 className="text-xl md:text-2xl">{productDetails?.title}</h2>
+              <p className="my-3 font-light text-sm md:text-base">{productDetails?.description}</p>
+              <h3 className="mb-2 text-sm md:text-lg">{productDetails?.category?.name}</h3>
               <div className="flex justify-between mx-4 mb-3">
-                <p>{productDetails?.price} EGY</p>
+                <p className="text-lg">{productDetails?.price} EGY</p>
                 <p className="flex items-center">
                   {productDetails?.ratingsAverage}
                   <FaStar className="text-yellow-400 ms-1" />
                 </p>
               </div>
               <button
-                onClick={() => addItem(productDetails._id)} // Changed to _id
-                className="w-full bg-green-600 rounded-sm py-1 text-white"
+                onClick={() => addItem(productDetails._id)}
+                className="w-full bg-green-600 rounded-sm py-2 text-sm md:text-base lg:text-lg text-white"
               >
                 Add to Cart
               </button>
             </div>
           </div>
-
         </>
       )}
     </>
